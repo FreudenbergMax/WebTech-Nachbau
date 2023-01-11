0f8deb2ed2b3ef2821ec26474c756e2cf4436033
FreudenbergMax <S0566289@htw-berlin.de>
2023-01-11 18:58:50 +0100

    Meilenstein 2.9: Entität Person angelegt

---
+++ src/main/java/de/htwberlin/webtech/webtechnachbau/persistence/PersonEntity.java
@@ -0,0 +1,75 @@
+package de.htwberlin.webtech.webtechnachbau.persistence;
+
+import javax.persistence.*;
+
+/*
+Es mag doppelt gemoppelt sein, dass es sowohl eine Klasse Person und eine Klasse PersonEntity gibt. Dies ist
+beabsichtigt, weil man eine Trennung zwischen Datendefinition (Person) und dem Datenmodell (PersonEntity) haben möchte.
+Person gehört der REST-Schnittstelle. Die PersonEntity bildet 1-zu-1 eine Tabelle ab.
+ */
+
+// Name der Tabelle lautet "persons"
+@Entity(name = "persons")
+public class PersonEntity {
+
+    // Jeder der Attribute ist eine Spalte in der Tabelle "persons"
+
+    @Id                                                     // bestimmt, dass id der Primärschlüssel ist
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // generiert automatisch einen numerischen Wert, der um 1 hoch zählt
+    @Column(name = "id")                                    // definiert Attribut als Tabellenspalte
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "vaccinated")
+    private Boolean vaccinated;
+
+    // Konstruktor wird benötigt, um Entitäten in der Anwendung erzeugen zu können
+    public PersonEntity(Long id, String firstName, String lastName, Boolean vaccinated) {
+        // id ist eigentlich überflüssig, da diese ja von der Datenbank generiert werden soll
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.vaccinated = vaccinated;
+    }
+
+    // Der parameterlose Konstruktor wird im Hintergrund benötigt, damit OR-Mapping mit Hibernate funktioniert
+    protected PersonEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(Boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+}
